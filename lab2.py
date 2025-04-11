cb08108008236e89e80ea6516a3644b6fbf4f8fb
hrnyadna <ariadna.illarionova@gmail.com>
2025-04-11 19:24:56 +0300

    Delete lab2.py

--- lab2.py
+++
@@ -1,7 +0,0 @@
-##5 вариант
-with open(r"IN 1.txt", "r") as file:
-    s = file.readline().strip()
-    t = file.readline().strip()
-for i in range(len(s) - len(t) + 1):
-    if s[i:i + len(t)] == t:
-         print(i + 1, end=" ")
